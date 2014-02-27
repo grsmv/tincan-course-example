7692834d719d84ecd345a2fd504ad827827e43f1
Sergey Gerasimov <mail@grsmv.com>
2014-02-27 02:16:02 +0200

    statements sended

--- javascripts/application.js
+++ javascripts/application.js
@@ -6,6 +6,8 @@
     Thank you and let the Force be with you.
 */
 
+window.tincan = tincanInit(); // Douglas Crockford, sorry
+
 function login () {
     var email    = $("#email").val(),
         name     = $("#name").val(),
@@ -16,6 +18,8 @@ function login () {
         localStorage.setItem("tincan-email",    email);
         localStorage.setItem("tincan-name",     name);
         localStorage.setItem("tincan-password", password);
+
+        window.tincan = tincanInit();
     } else {
         alert ("email, name or password can not be empty");
     }
@@ -24,21 +28,64 @@ function login () {
 function logout () {
     localStorage.clear();
     $("body").addClass("no-access");
+    window.tincan = {};
 }
+
+function tincanInit () {
+    return new TinCan ({
+        recordStores: [{
+            endpoint: "http://localhost:8000/",
+            username: localStorage.getItem("tincan-email"),
+            password: localStorage.getItem("tincan-password")
+        }]
+    });
+}
+
 
 (function(){
 
     // side menu init
     $(".menu-toggler").sidr();
 
+    // user state checking
     if (typeof(Storage) !== "undefined") {
-        var email    = localStorage.getItem("tincan-email"),
-            name     = localStorage.getItem("tincan-name"),
-            password = localStorage.getItem("tincan-password");
-
-        // unauthorized user
-        if (email === null || password === null || name === null) {
+        if (localStorage.getItem("tincan-email")    === null ||
+            localStorage.getItem("tincan-password") === null ||
+            localStorage.getItem("tincan-name")     === null) {
             $("body").addClass("no-access");
+        } else {
+
+            // ------------ activity description -------------
+
+            tincan.sendStatement({
+                actor: {
+                    mbox: localStorage.getItem("tincan-email"),
+                    name: localStorage.getItem("tincan-name")
+                },
+                verb: {
+                    id: "http://adlnet.gov/expapi/verbs/attempted",
+                    display: {
+                        "en-US": "attempted"
+                    }
+                },
+                object: {
+                    id: "http://course.tincanlrs.net",
+                    objectType: "Activity",
+                    definition: {
+                        name: {
+                            "en-US": "Section opening"
+                        },
+                        description: {
+                            "en-US": window.location.toString()
+                        }
+                    }
+                }
+            });
+
+            // ------------ activity description -------------
+
+            // initializing scroll depth tracking
+            $.scrollDepth();
         }
     } else {
         alert("Please, use modern browser");
